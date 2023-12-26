e620b700d21df25ca07b57f191db94b15a643dee
Toyehamzat <anibiseun@gmail.com>
2023-12-26 10:33:21 +0000

    updated index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,12 @@
 "use client";
+import Billboard from "@/components/Billboard";
 import Navbar from "@/components/navbar";
-import { signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
-  const { data, status } = useSession();
-  const router = useRouter();
-
-  const logouthandler = async () => {
-    await signOut();
-  };
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/auth");
-    }
-  }, [status, router]);
-
   return (
     <div className="flex flex-col">
       <Navbar />
-      {/* <div className="text-white">WELCOME {data?.user?.name}</div>
-      {status === "authenticated" && (
-        <button
-          className="text-black text-4xl bg-white"
-          onClick={logouthandler}
-        >
-          LOGOUT
-        </button>
-      )} */}
+      <Billboard />
     </div>
   );
 }
