4716d22ef2642042d6a4509533acc8717bfa621c
Toyehamzat <anibiseun@gmail.com>
2023-12-28 19:45:31 +0000

    added My list component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/navbar";
+import useFavorites from "@/hooks/useFavorites";
 import useMovies from "@/hooks/useMovies";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -18,12 +19,17 @@ export default function Home() {
   }, [status, router]);
 
   const { data: movies = [] } = useMovies();
+  const { data: Favorites = [] } = useFavorites();
   return (
     <div className="flex flex-col">
       <Navbar />
       <Billboard />
       <div className="pb-40 text-white">
         <MovieList title="Trending now" data={movies} />
+        {/* <MovieList title="My list" data={Favorites} /> */}
+        <MovieList title="For you" data={movies} />
+        <MovieList title="Kids" data={movies} />
+        <MovieList title="family" data={movies} />
       </div>
     </div>
   );
