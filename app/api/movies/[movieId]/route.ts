24d316bbe5ad0dbe3d64005d7ad70242fe97ae7c
Toyehamzat <anibiseun@gmail.com>
2024-04-09 22:45:42 +0100

    Refactor movie route to use named export for GET method

--- app/api/movies/[movieId]/route.ts
+++ app/api/movies/[movieId]/route.ts
@@ -1,53 +1,13 @@
-// // watch/[movieId]/route.ts
-// import { NextApiRequest, NextApiResponse } from "next";
-// import prisma from "@/prisma";
-// import serverAuth from "@/lib/serverAuth";
-
-// export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-//   try {
-//     if (req.method !== "GET") {
-//       return res.status(405).end();
-//     }
-
-//     await serverAuth();
-
-//     const { movieId } = req.query;
-
-//     if (!movieId || typeof movieId !== "string") {
-//       throw new Error("Invalid or missing movieId");
-//     }
-
-//     const movie = await prisma.movie.findUnique({
-//       where: {
-//         id: movieId,
-//       },
-//     });
-
-//     if (!movie) {
-//       throw new Error("Movie not found");
-//     }
-
-//     return res.status(200).json(movie);
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
-
 // watch/[movieId]/route.ts
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/prisma";
 import serverAuth from "@/lib/serverAuth";
 
-// Change the function signature to match the correct types
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
   try {
-    if (req.method !== "GET") {
-      return res.status(405).end();
-    }
+    // if (req.method !== "GET") {
+    //   return res.status(405).end();
+    // }
 
     await serverAuth();
 
